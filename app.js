718522da0ab86858ccc6617d21e9c0ef057f5aa9
sohan9819 <sohanshetty2001@gmail.com>
2021-09-18 21:41:07 +0530

    Fixed negative input

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-const form = document.querySelector('#form')
-const result = document.querySelector('#result')
+const form = document.querySelector("#form");
+const result = document.querySelector("#result");
 
-form.addEventListener('submit' , function(e){
-    e.preventDefault()
-    console.log(this.date.value)
-    console.log(this.number.value)
-    const date = this.date.value
-    const number = this.number.value
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(this.date.value);
+  console.log(this.number.value);
+  const date = this.date.value;
+  const number = this.number.value;
 
-    if ( date === "" || number === ""){
-        result.innerText = "Please enter all the fields"
+  if (date === "" || number === "") {
+    result.innerText = "Please enter all the fields";
+  } else if (number < 0) {
+    result.innerText = "Lucky number connot be negative";
+  } else {
+    date.toString();
+    luckyCheck(date, number);
+  }
+});
+
+function luckyCheck(date, number) {
+  let sum = 0;
+  for (let i = 0; i < date.length; i++) {
+    let num = date.charAt(i);
+    if (isNaN(num) === false) {
+      console.log(num);
+      sum = sum + parseInt(num);
+      console.log("sum : " + sum);
     }
-    else {
-        date.toString()
-        luckyCheck(date , number)
-    }
-})
+  }
 
-function luckyCheck(date , number){
-
-    let sum = 0
-    for(let i = 0 ; i < date.length; i++){
-        let num = date.charAt(i);
-        if(isNaN(num) === false) {
-            console.log(num)
-            sum=sum+parseInt(num)
-            console.log("sum : " + sum )
-        }
-    }
-
-    if ( sum%number == 0){
-        result.innerText=`${number} is your lucky number!!! 🥳🥳🥳`
-    } else {
-        result.innerText=`${number} is not that lucky for you`
-    }
-
+  if (sum % number == 0) {
+    result.innerText = `${number} is your lucky number!!! 🥳🥳🥳`;
+  } else {
+    result.innerText = `${number} is not that lucky for you`;
+  }
 }
